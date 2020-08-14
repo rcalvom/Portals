ff254df1215d9f38c9c1b766fe6d7f8271837673
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-08-14 14:55:02 -0500

    Arreglo bug de salto.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -35,9 +35,9 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move*speed*Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButton("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
